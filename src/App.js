9b25715611a206d27ae9b5072e87cfc7a9939556
DyogoViana <dyogo.vianaa@gmail.com>
2021-03-22 20:55:28 -0300

    Início do projeto.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Follwo the White Rabbit</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
